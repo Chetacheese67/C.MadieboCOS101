db03400af2de1803ea8d6e5005376a43ff0cb790
Chetachi Madiebo <hecka@9I6J72AL.2859516740-777710090-106874645>
2025-10-17 10:43:45 -0700

    (boolean)

---
+++ week-3/practice_2/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+	let school_string = "School of Science and Technology";  // string type
+	let rating_float = 5.0;    //float type
+let is_growing_boolean = true //boolean type
+let department_intger = 3;   //integer type
+
+println!("Schhol Name is: {}",school_string);
+println!("Schhol rating on 5 is: {}",rating_float);
+println!("School is growing :{}", is_growing_boolean);
+println!("Number of Departments :{}", department_intger);
+}
