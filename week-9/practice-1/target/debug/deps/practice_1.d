2af8cc2ababa698bb1073728f3471ecfe98358da
Chetachi Madiebo <hecka@9I6J72AL.2859516740-777710090-106874645>
2025-11-26 03:27:09 -0800

    (Writing to a file fgor the first time)

---
+++ week-9/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\hecka\Documents\C.MadieboCOS101\week-9\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\hecka\Documents\C.MadieboCOS101\week-9\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
