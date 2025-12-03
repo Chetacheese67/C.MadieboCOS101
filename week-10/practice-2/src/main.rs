3952dffa833352ccdcf7ca132f13e9717ada7c6c
Chetachi Madiebo <hecka@9I6J72AL.2859516740-777710090-106874645>
2025-12-03 13:26:47 +0100

    (passing values to a function)

--- week-10/practice-2/src/main.rs
+++ week-10/practice-2/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![10, 20, 30];
+    // v owns the object in the heap
+
+    let v2 = v;
+    // ownership moved from v to v2
+
+    display(&v2); 
+    // we borrow v2 instead of moving it
+
+    println!("In main {:?}", v2);
+    // v2 is still usable because it was only borrowed
+}
+
+fn display(v: &Vec<i32>) {
+    println!("inside display {:?}", v);
 }
